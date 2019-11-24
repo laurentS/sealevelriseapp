40dcfe99bd9221fe6d9511207b828335a7908c3d
Laurent Savaete <laurent@where.tf>
2019-11-24 16:25:44 +0000

    Updates

---
+++ precache-manifest.6e35c91f8463d9d59dee04b336300701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a107687eecd7cfa2367392592cae512",
+    "url": "/sealevelriseapp/index.html"
+  },
+  {
+    "revision": "1d0e272b9746604ff8ec",
+    "url": "/sealevelriseapp/static/css/main.13606ce6.chunk.css"
+  },
+  {
+    "revision": "6ba352d6703eb73c55b1",
+    "url": "/sealevelriseapp/static/js/2.914d2d5f.chunk.js"
+  },
+  {
+    "revision": "1d0e272b9746604ff8ec",
+    "url": "/sealevelriseapp/static/js/main.dbe044a8.chunk.js"
+  },
+  {
+    "revision": "15c1b4251dcc4b21549b",
+    "url": "/sealevelriseapp/static/js/runtime-main.e901adab.js"
+  }
+]);
